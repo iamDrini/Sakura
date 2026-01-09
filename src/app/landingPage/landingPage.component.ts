9d67729b3df2c6379be2c7b33ef6f416a4437727
iamDrini <valdrin.murselji@outlook.de>
2026-01-10 00:19:01 +0100

    init sakura

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+import { Navbar } from './navbar/navbar';
+
+@Component({
+    selector: 'app-landingPage',
+    standalone: true,
+    imports:[Navbar],
+    template: `
+    <section>
+        <div><app-navbar></app-navbar></div>
+        <h1 class="fontRaleway">SAKURA RAMEN</h1>
+        <h2 clasS="fontRaleway">BEST RAMEN IN TOWN</h2>
+    </section>
+    `,
+    styleUrls:['./landingPage.component.scss']
+})
+
+export class LandingPageComponent{
+
+}
