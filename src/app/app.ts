6f751e7073b950bb209b857d3d65cee4339e3c1d
iamDrini <valdrin.murselji@outlook.de>
2026-01-11 15:33:05 +0100

    refactoring & routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainContent } from './main-content/main-content';
 import { Footer } from './shared/components/footer/footer';
-import { Imprint } from './imprint/imprint';
+
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
-    MainContent,
-    Footer,
-    Imprint],
+    Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
